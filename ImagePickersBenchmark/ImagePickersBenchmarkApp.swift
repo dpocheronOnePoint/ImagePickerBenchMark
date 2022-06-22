c867c6906370418f6cf312f0f5c1785a7873bb45
your_username <your_email@gmail.com>
2022-06-22 10:04:54 +0200

    Initial Commit

---
+++ ImagePickersBenchmark/ImagePickersBenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickersBenchmarkApp.swift
+//  ImagePickersBenchmark
+//
+//  Created by Dimitri POCHERON on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickersBenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
